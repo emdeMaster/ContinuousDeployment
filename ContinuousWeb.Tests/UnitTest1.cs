148f315f60f6ea5ad4cfc6a75bf70d24b5f6d270
dekiert.marcin <dekiert.marcin@gmail.com>
2018-02-25 21:54:05 +0100

    Added test project

---
+++ ContinuousWeb.Tests/UnitTest1.cs
@@ -0,0 +1,16 @@
+using ContinuousDeployment.Controllers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ContinuousWeb.Tests
+{
+    [TestClass]
+    public class HomeControllerTests
+    {
+        [TestMethod]
+        public void AboutPageTest()
+        {
+            var homeController = new HomeController();
+            var result = homeController.About();
+        }
+    }
+}
